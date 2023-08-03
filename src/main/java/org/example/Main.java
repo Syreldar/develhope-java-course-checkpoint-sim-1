399934984934cac9df369fbb88e9fa7826a76175
Syreldar <enrico.drago3@gmail.com>
2023-08-03 13:35:41 +0200

    Upload exercise

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class Main
+{
+    public static final int NUM_LETTERS = 3;
+
+    public static void printFirstNLetters(String[] arr, int numLetters)
+    {
+        for (String s : arr)
+        {
+            if (s.length() >= numLetters)
+                System.out.printf("%s%n", s.substring(0, numLetters));
+        }
+    }
+
+    public static String NthLettersChains(String[] arr, int n, int numLetters)
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i].length() >= numLetters)
+                result.append(arr[i].charAt(numLetters-1));
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args)
+    {
+        Person[] people = new Person[]
+        {
+            new Person("Enrico", "Drago", 25),
+            new Person("Rossella", "Ferraù", 30),
+            new Person("Francesco", "Barazzuti", 35)
+        };
+
+        String[] lastNames = new String[people.length];
+        for (int i = 0; i < people.length; i++)
+        {
+            lastNames[i] = people[i].getLastName();
+        }
+
+        printFirstNLetters(lastNames, NUM_LETTERS);
+        System.out.printf("%s%n", NthLettersChains(lastNames, 2, NUM_LETTERS));
+
+        for (Person person : people)
+        {
+            System.out.printf("%d%n", person.getAge());
+        }
+    }
+}
